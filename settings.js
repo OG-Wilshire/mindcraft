92a5aeb1e824cc426d2901ae0e4c3f07a879fbb9
OG-Wilshire <59239191+OG-Wilshire@users.noreply.github.com>
2024-11-24 00:03:13 -0600

    Update settings.js
    Enable memory.
    Disable verbose talk.

--- settings.js
+++ settings.js
@@ -16,18 +16,18 @@ export default
         
         // using more than 1 profile requires you to /msg each bot indivually
     ],
-    "load_memory": false, // load memory from previous session
+    "load_memory": true, // load memory from previous session
     "init_message": "Say hello world and your name", // sends to all on spawn
 
     "language": "en", // translate to/from this language. Supports these language names: https://cloud.google.com/translate/docs/languages
     "show_bot_views": false, // show bot's view in browser at localhost:3000, 3001...
 
     "allow_insecure_coding": false, // allows newAction command and model can write/run code on your computer. enable at own risk
-    "code_timeout_mins": 10, // minutes code is allowed to run. -1 for no timeout
+    "code_timeout_mins": 1, // minutes code is allowed to run. -1 for no timeout
 
     "max_messages": 15, // max number of messages to keep in context
     "max_commands": -1, // max number of commands to use in a response. -1 for no limit
-    "verbose_commands": true, // show full command syntax
-    "narrate_behavior": true, // chat simple automatic actions ('Picking up item!')
+    "verbose_commands": false, // show full command syntax
+    "narrate_behavior": false, // chat simple automatic actions ('Picking up item!')
 }
 
